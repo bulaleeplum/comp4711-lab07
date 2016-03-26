dcfaf4bfdb2e3724c95bed3da4997495cbd3d00b
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-03-26 12:51:50 -0700

    Populate model information for courses, timeslots, and days

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -6,21 +6,72 @@
      * Time: 3:03 PM
      */
 class Timetable extends CI_Model {
-    protected $xml;
-    protected $daysofweek;
+    private $xml;
+    private $daysOfWeek;
+    private $courses;
+    private $timeslots;
 
     public function __construct() {
         $this->xml = simplexml_load_file('data/timetable.xml');
-        print_r($this->xml->days);
-        foreach($this->xml->days as $day) {
-            echo $day;
+
+        //populate daysOfWeek array from days.xml
+        foreach($this->xml->days->day as $day) {
+            foreach($day->booking as $bookings) {
+                $booking = array();
+                $booking['room'] = (string) $bookings->room;
+                $booking['dayofweek'] = (string) $day['code'];
+                $booking['time'] = (string) $bookings->time;
+                $booking['coursename'] = (string) $bookings->coursename;
+                $booking['instr'] = (string) $bookings->instr;
+                $booking['type'] = (string) $bookings->type;
+                $this->daysOfWeek[] = new Booking($booking);
+
+            }
         }
+
+        //populates courses array from courses.xml
+        foreach($this->xml->courses->course as $course) {
+            foreach($course->booking as $bookings) {
+                $booking = array();
+                $booking['room'] = (string) $bookings->room;
+                $booking['dayofweek'] = (string) $bookings->dayofweek;
+                $booking['time'] = (string) $bookings->time;
+                $booking['coursename'] = (string) $course['name'];
+                $booking['instr'] = (string) $bookings->instr;
+                $booking['type'] = (string) $bookings->type;
+                $this->courses[] = new Booking($booking);
+
+            }
+        }
+
+        //populates timeslots array from timeslots.xml
+        foreach($this->xml->timeslots->timeslot as $timeslot) {
+            foreach($timeslot->booking as $bookings) {
+                $booking = array();
+                $booking['room'] = (string) $bookings->room;
+                $booking['dayofweek'] = (string) (string) $bookings->dayofweek;
+                $booking['time'] = (string) $timeslot['time'];
+                $booking['coursename'] = (string) $bookings->coursename;
+                $booking['instr'] = (string) $bookings->instr;
+                $booking['type'] = (string) $bookings->type;
+                $this->timeslots[] = new Booking($booking);
+
+            }
+        }
+
     }
 
-    public function getDaysOfWeek() {
-        return $this->daysoftheweek;
+    public function getDaysOfTheWeek() {
+        return $this->daysOfWeek;
     }
 
+    public function getCourses() {
+       return $this->courses;
+    }
+
+    public function getTimeslots() {
+        return $this->timeslots;
+    }
 
 }
 
