394fb5f2e55d1e27d527ba8e2d37f800efb5d881
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-03-27 17:33:30 -0700

    Add search methods in model and controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -28,4 +28,38 @@ class Welcome extends MY_Controller {
 
 		$this->render();
 	}
+
+    /*
+     * Get inputs from search form and get the results from the Timetable model.
+     */
+	function search () {
+        // Search form inputs
+		//$queryDay = $this->input->post('day');
+		//$queryTime= $this->input->post('timeslot');
+
+        // Testing
+		$queryDay = 'Monday';
+		$queryTime = '15:30';
+
+		$dayResult = $this->timetable->searchDaysOfTheWeek($queryDay, $queryTime);
+		$courseResult = $this->timetable->searchCourses($queryDay, $queryTime);
+		$timeslotResult = $this->timetable->searchTimeslots($queryDay, $queryTime);
+
+        // Check if the search returns consistent data. If consistent, display 'bingo' and result.
+        // Each facet should return a single booking and the three bookings should be the same.
+        if (count($dayResult == 1 &&
+                  $courseResult == 1 &&
+                  $timeslotResult == 1) &&
+            ($dayResult == $courseResult && $courseResult == $timeslotResult)) {
+            $this->data['bingo'] == "Badabing badaboom, wawaweewa, it's a good.";
+
+        } else {
+            $this->data['bingo'] == "Error, it's a bad.";
+        }
+        // Pass view parameters:
+        // Set $dayResult
+        // Set $courseResult
+        // Set $timeSlotResult
+        $this->render();
+    }
 }
